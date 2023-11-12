bed47bf2b02001891eef24cd689ed0a9a9b2931e
Anjuman Raj <ayusmann23@gmail.com>
2023-11-12 09:01:36 +0530

    hero image update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <>
-        <div className="flex flex-col md:flex-row justify-between items-center px-2 md:px-10 lg:px-20 gap-y-9 pt-4">
+        <div className="hidden md:flex flex-col md:flex-row justify-between items-center px-2 md:px-10 lg:px-20 gap-y-9 pt-4 ">
             <Link href='/' > <div className=" font-borel text-2xl mt-3 cursor-pointer"> anjuman raj </div></Link>
             <div className="hidden  md:flex flex-row gap-x-4 ">
                 <div className="">
