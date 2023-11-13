5d734a537c8258e2d30663e93097b10c8b6779cb
Anjuman Raj <ayusmann23@gmail.com>
2023-11-13 10:35:46 +0530

    hero image fix

--- src/components/home/Hero.tsx
+++ src/components/home/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
                 </div>
             </div>
             <div className=" flex justify-center lg:justify-end   ">
-                <Image loading='eager' src='/images/sky.jpg' width={300} height={700} alt='ofc my pic' className='rounded-md shadow-white  cursor-pointer ' />
+                <Image loading='eager' src='/images/anjuman.png' width={300} height={700} alt='ofc my pic' className='rounded-md shadow-white  cursor-pointer ' />
             </div>
         </div>
     )
