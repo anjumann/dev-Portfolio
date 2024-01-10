b98acc4e1f0eaf9308fc9e324504310ce5629805
Anjuman Raj <ayusmann23@gmail.com>
2024-01-10 18:01:11 +0530

    mdx integration done

---
+++ src/app/docs/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Docs from '@/components/mdx/content/docs/Content.mdx'
+const page = () => {
+  return (
+    <div className='prose dark:prose-invert prose-slate screen-width '  >
+        <Docs />
+    </div>
+  )
+}
+
+export default page
