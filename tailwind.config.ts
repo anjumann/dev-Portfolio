e8c0d5c9a521e9283de70fd4caaeaba8e27f2dba
Anjuman Raj <ayusmann23@gmail.com>
2024-01-10 12:48:06 +0530

    🔧 chore(components.json): update tailwind.config.js to tailwind.config.ts 📝 docs(tailwind.config.ts): add tailwind configuration file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,5 +76,5 @@ module.exports = {
       }
     },
   },
-  plugins: [require("tailwindcss-animate"), require('@tailwindcss/aspect-ratio')],
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/aspect-ratio'), require('@tailwindcss/typography')],
 }
