63ccc3797507bc69ee24e7dc4720f3597ef009a1
Anjuman Raj <ayusmann23@gmail.com>
2024-01-10 18:54:05 +0530

    🎨 style(page.tsx): update className to include min-height and margin-top properties 📝 docs(Content.mdx): update content of the MDX page with a sample blog post

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import Docs from '@/components/mdx/content/docs/Content.mdx'
 const page = () => {
   return (
-    <div className='prose dark:prose-invert prose-slate screen-width '  >
+    <div className='prose dark:prose-invert prose-slate min-h-[80vh] screen-width  mt-7'  >
+
+
         <Docs />
+
     </div>
   )
 }
