7e4de31bc88031550b1bae17b1b3055c9db2344e
Anjuman Raj <ayusmann23@gmail.com>
2024-01-13 08:38:45 +0530

    🔧 fix(page.tsx): remove unnecessary class name 'screen-width' from div element 🔧 fix(Content.mdx): remove unnecessary div element wrapping code block

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 import Docs from '@/components/mdx/content/docs/Content.mdx'
 const page = () => {
   return (
-    <div className='prose dark:prose-invert prose-slate min-h-[80vh] screen-width  mt-7'  >
-
-
+    <div className='prose dark:prose-invert min-h-[80vh] screen-width  mt-7'  >
         <Docs />
-
     </div>
   )
 }
